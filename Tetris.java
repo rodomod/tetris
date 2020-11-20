55104d28efcb2a8a05d1d22127672ff590c6f37f
rodomod <odministrator@hotmail.com>
2020-11-20 18:25:45 +0400

    tetris new Tetris();

--- Tetris.java
+++ Tetris.java
@@ -1,26 +1,29 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import java.util.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Tetris extends JFrame {
-    /**
-	 * @Author ... rodomod ...
-	 */
-	
-	final String TITLE_OF_PROGRAM = "Tetris";
-    final int BLOCK_SIZE = 25; // size of one block
+class Tetris extends JFrame {
+
+    final String TITLE_OF_PROGRAM = "Tetris";
+    final int BLOCK_SIZE = 25;   //размер одного блока size of one block  
     final int ARC_RADIUS = 6;
-    final int FIELD_WIDTH = 10; // size game field in block
-    final int FIELD_HEIGHT = 18;
+    final int FIELD_WIDTH = 10;   //размер игрового поля в блоке size game field in block
+    final int FIELD_HEIGHT = 21;  //18
     final int START_LOCATION = 180;
-    final int FIELD_DX = 7; // determined experimentally
+    final int FIELD_DX = 7;   //определено экспериментально determined experimentally
     final int FIELD_DY = 26;
-    final int LEFT = 37; // key codes
+    final int LEFT = 37;    //коды клавиш key codes
     final int UP = 38;
     final int RIGHT = 39;
     final int DOWN = 40;
-    final int SHOW_DELAY = 400; // delay for animation
+    final int SHOW_DELAY = 400;    //задержка анимации delay for animation
     final int[][][] SHAPES = {
         {{0,0,0,0}, {1,1,1,1}, {0,0,0,0}, {0,0,0,0}, {4, 0x00f0f0}}, // I
         {{0,0,0,0}, {0,1,1,0}, {0,1,1,0}, {0,0,0,0}, {4, 0xf0f000}}, // O
@@ -32,7 +35,7 @@ public class Tetris extends JFrame {
     };
     final int[] SCORES = {100, 300, 700, 1500};
     int gameScore = 0;
-    int[][] mine = new int[FIELD_HEIGHT + 1][FIELD_WIDTH]; // mine/glass
+    int[][] mine = new int[FIELD_HEIGHT + 1][FIELD_WIDTH];  //мой / стекло  mine/glass 
     JFrame frame;
     Canvas canvas = new Canvas();
     Random random = new Random();
@@ -61,7 +64,8 @@ public class Tetris extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setBounds(START_LOCATION, START_LOCATION, FIELD_WIDTH * BLOCK_SIZE + FIELD_DX, FIELD_HEIGHT * BLOCK_SIZE + FIELD_DY);
         setResizable(false);
-        canvas.setBackground(Color.cyan);           //(Color.black); <--define the background color
+		setLocationRelativeTo(null);  //игра в центре экрана
+        canvas.setBackground(Color.black); // определить цвет фона define the background color
         addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
                 if (!gameOver) {
@@ -74,10 +78,10 @@ public class Tetris extends JFrame {
         });
         add(BorderLayout.CENTER, canvas);
         setVisible(true);
-        Arrays.fill(mine[FIELD_HEIGHT], 1); // create a ground for mines
+        Arrays.fill(mine[FIELD_HEIGHT], 1);   //создаем площадку для мин create a ground for mines
     }
 
-    void go() { // main loop of game
+    void go() { //основной цикл игры  main loop of game
         while (!gameOver) {
             try {
                 Thread.sleep(SHOW_DELAY);
@@ -87,13 +91,13 @@ public class Tetris extends JFrame {
             if (figure.isTouchGround()) {
                 figure.leaveOnTheGround();
                 figure = new Figure();
-                gameOver = figure.isCrossGround(); // Is there space for a new figure?
+                gameOver = figure.isCrossGround(); //Есть ли место для новой фигуры? Is there space for a new figure?
             } else
                 figure.stepDown();
         }
     }
 
-    void checkFilling() { // check filling rows
+    void checkFilling() { //проверка заполнения строк  check filling rows
         int row = FIELD_HEIGHT - 1;
         int countFillRows = 0;
         while (row > 0) {
@@ -111,12 +115,13 @@ public class Tetris extends JFrame {
             setTitle(TITLE_OF_PROGRAM + " : " + gameScore);
         }
     }
-
+	
+/////////////////////////////_FIGURE
     class Figure {
         private ArrayList<Block> figure = new ArrayList<Block>();
         private int[][] shape = new int[4][4];
         private int type, size, color;
-        private int x = 3, y = 0; // starting left up corner
+        private int x = 3, y = 0; //начало в левом верхнем углу  starting left up corner 
 
         Figure() {
             type = random.nextInt(SHAPES.length);
@@ -185,13 +190,13 @@ public class Tetris extends JFrame {
         void rotateShape(int direction) {
             for (int i = 0; i < size/2; i++)
                 for (int j = i; j < size-1-i; j++)
-                    if (direction == RIGHT) { // clockwise
+                    if (direction == RIGHT) { //по часовой стрелке  clockwise
                         int tmp = shape[size-1-j][i];
                         shape[size-1-j][i] = shape[size-1-i][size-1-j];
                         shape[size-1-i][size-1-j] = shape[j][size-1-i];
                         shape[j][size-1-i] = shape[i][j];
                         shape[i][j] = tmp;
-                    } else { // counterclockwise
+                    } else { //против часовой стрелки  counterclockwise
                         int tmp = shape[i][j];
                         shape[i][j] = shape[j][size-1-i];
                         shape[j][size-1-i] = shape[size-1-i][size-1-j];
@@ -213,8 +218,10 @@ public class Tetris extends JFrame {
             for (Block block : figure) block.paint(g, color);
         }
     }
+	
+//////////////////////////////////////_BLOCK
 
-    class Block { // building element for Figure
+    class Block { //строительный элемент для рисунка  building element for Figure
         private int x, y;
 
         public Block(int x, int y) {
@@ -233,8 +240,11 @@ public class Tetris extends JFrame {
             g.drawRoundRect(x*BLOCK_SIZE+1, y*BLOCK_SIZE+1, BLOCK_SIZE-2, BLOCK_SIZE-2, ARC_RADIUS, ARC_RADIUS);
         }
     }
+	
+	
+/////////////////////////////////////_CANVAS
 
-    class Canvas extends JPanel { // my canvas for painting
+    class Canvas extends JPanel { //мой холст для рисования  my canvas for painting
         @Override
         public void paint(Graphics g) {
             super.paint(g);
